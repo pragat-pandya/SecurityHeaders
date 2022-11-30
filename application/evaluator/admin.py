49ba9c64b988a9ea6ff524727a7381e087bd81c9
Pragat Pandya <pragat.pandya@gmail.com>
2022-11-30 22:07:50 +0530

    Define Models for leads and scans

--- application/evaluator/admin.py
+++ application/evaluator/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class LeadsManager (admin.ModelAdmin):
+  list_display = ('first_name', 'last_name', 'email', 'date_time')
+
+class ScansManager (admin.ModelAdmin):
+  list_display = ('ip', 'url', 'date_time', 'is_private', 'client_details')
+
+
+admin.site.register(Lead, LeadsManager)
+admin.site.register(Scan, ScansManager)
--- application/evaluator/models.py
+++ application/evaluator/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Lead (models.Model):
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True, primary_key=True)
+  date_time = models.DateTimeField(auto_now_add=True)
+
+
+class Scan (models.Model):
+  url = models.URLField(max_length=150, db_index=True, blank=True)
+  ip = models.GenericIPAddressField()
+  date_time = models.DateTimeField(auto_now_add=True, primary_key=True)
+  is_private = models.BooleanField(default=False)
+  client_details = models.ForeignKey(Lead, on_delete=models.DO_NOTHING, null=True)
